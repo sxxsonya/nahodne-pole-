999ad5a4405f958f26e603dfeac82ae5f72e75ac
Sofiia Shokalenko <sofiashokalenko@gmail.com>
2024-05-06 09:27:34 +0200

    nahodnepolepyypypypyp

---
+++ nahodne_pole.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def generate_random_word(lenght):
+    word = ""
+    for _ in range(lenght):
+        word +=
+random.choice(string.ascii.lowercase)
+    return_word
+word_lenght = random.randint(1, 10)
+random_word =
+generate_random_word(word.lenght)
+print(random_word)
